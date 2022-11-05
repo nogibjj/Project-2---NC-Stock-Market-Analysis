2da82640c1acda7396e4484866e0465e18c52289
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-11-05 17:20:43 +0000

    Added input to streamlit app.

--- stapp.py
+++ stapp.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title(r"Nick Carroll's Efficient Frontier App")
 
+stocks = st.text_input('Please input the stock tickers of the portfolio that you are looking to optimize:', 'AMZN AAPL MSFT')
+
+st.subheader(stocks)
